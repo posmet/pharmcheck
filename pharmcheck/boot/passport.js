13c2ef9e3a1a1a58388060ac73759d0527d5bf99
Александр Посметный <posmet@yandex.ru>
2019-01-13 13:07:54 +0300

    auth

--- pharmcheck/boot/passport.js
+++ pharmcheck/boot/passport.js
@@ -7,22 +7,18 @@ const AuthLocalStrategy = require('passport-local').Strategy;
 passport.use('local', new AuthLocalStrategy(
     function (username, password, done) {
 
-      let sqlString = "SELECT u.userid,user_name,full_name,p.ph_id,ph_name from ref_users u inner join roles r on r.userid=u.userid inner join pharms p on p.ph_id = r.ph_id where user_name='" + username + "' and pwd = '" + password + "'";
-      const request = new sql.Request(pool);
-      console.log(sqlString);
-      request.query(sqlString, function (err, rs) {
+      //let sqlString = "SELECT u.userid,user_name,full_name,p.ph_id,ph_name from ref_users u inner join roles r on r.userid=u.userid inner join pharms p on p.ph_id = r.ph_id where user_name='" + username + "' and pwd = '" + password + "'";
+      //const request = new sql.Request(pool);
+      //console.log(sqlString);
+      //request.query(sqlString, function (err, rs) {
         if (err) {
           console.log(err);
           return done(err);
         }
-        if (rs.recordset.length > 0) {
+        if ((username === 'admin') && (password='afhvfcjan')) {
           return done(null, {
             username: username,
-            full_name: rs.recordset[0].full_name,
             profileUrl: "url_to_profile",
-            userid: rs.recordset[0].userid,
-            ph_id: rs.recordset[0].ph_id,
-            ph_name: rs.recordset[0].ph_name
           });
         }
         return done(null, null, 'Неверный логин или пароль');
---
+++ pharmcheck/routes/auth.js
@@ -0,0 +1,89 @@
+﻿//routes/auth.js
+const passport = require('passport');
+const messageService = require('../services/Message');
+const authService = require('../services/Auth');
+const middleware = require('../services/Middleware');
+
+const sql = require('mssql');
+const pool = require('../boot/sql');
+const nconf = require('nconf');
+
+const testReq = async () => {
+  let sqlString = `WAITFOR DELAY '00:00:0${Math.floor(Math.random() * 3)}'; SELECT * from roles`;
+  const request = new sql.Request(pool);
+  return await request.query(sqlString);
+};
+
+const testReq2 = async () => {
+  let sqlString = `WAITFOR DELAY '00:00:0${Math.floor(Math.random() * 3)}'; SELECT * from roles`;
+  try {
+    let connection = await new sql.ConnectionPool(nconf.get("mssql")).connect();
+    return await connection.request()
+      .query(sqlString);
+  } catch(e) {
+    throw e;
+  }
+};
+
+module.exports = function (app) {
+
+  app.get('/api/logout', function (req, res) {
+    req.logout();
+    req.user = {};
+    res.redirect('/');
+  });
+
+  app.post('/api/auth', function(req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+      if (err) return messageService.sendMessage(res, err);
+      if (!user) return messageService.sendMessage(res, "Неверный логин или пароль");
+
+      res.json({
+        token: authService.signToken(user)
+      });
+    })(req, res, next);
+  });
+
+  app.get('/api/test', middleware.asyncMiddleware(async (req, res, next) => {
+    let begin = new Date().getTime();
+    const promises = [];
+    for (let i = 0; i < 2; i++) {
+      promises.push(testReq);
+    }
+    let errors = 0;
+    let errors2 = 0;
+    let results = await Promise.all(promises.map(async (fn) => await fn().catch(() => errors++)));
+    results.forEach(r => {
+      if (!r.recordset) {
+        errors2++;
+      } else if (!r.recordset.length) {
+        errors2++;
+      }
+    });
+    const time = (new Date().getTime() - begin) / 1000;
+    console.log(time, errors, errors2, results.length);
+    res.json({errors, errors2, time, ln: results.length});
+  }));
+
+  app.get('/api/test2', middleware.asyncMiddleware(async (req, res, next) => {
+    let begin = new Date().getTime();
+    const promises = [];
+    for (let i = 0; i < 10000; i++) {
+      promises.push(testReq2);
+    }
+    let errors = 0;
+    let errors2 = 0;
+    let results = await Promise.all(promises.map(async (fn) => await fn()));
+    results.forEach(r => {
+      if (!r.recordset) {
+        errors2++;
+      } else if (!r.recordset.length) {
+        errors2++;
+      }
+    });
+    const time = (new Date().getTime() - begin) / 1000;
+    console.log(time, errors, errors2, results.length);
+    res.json({errors, errors2, time, ln: results.length});
+  }));
+
+};
